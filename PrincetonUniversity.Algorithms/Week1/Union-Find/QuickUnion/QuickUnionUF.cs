3be94803f6206a3b5e624083ad8405cf62e25860
Casimiro Greco <casimirogreco@gmail.com>
2021-12-11 12:53:40 +0100

    Quick Union with weighting and flattering tree

--- PrincetonUniversity.Algorithms/Week1/Union-Find/QuickUnion/QuickUnionUF.cs
+++ PrincetonUniversity.Algorithms/Week1/Union-Find/QuickUnion/QuickUnionUF.cs
@@ -9,6 +9,7 @@ namespace PrincetonUniversity.Algorithms.Part1.Week1.Union_Find.QuickUnion
     public class QuickUnionUF
     {
         private int[] id;
+        private int[] sizeRoot;
 
         //In the constructor, we initialize the main array with the starting index
         public QuickUnionUF(int n)
@@ -37,14 +38,59 @@ namespace PrincetonUniversity.Algorithms.Part1.Week1.Union_Find.QuickUnion
         public void Union(int p, int q)
         {
 
-            //We don't check that because the find in the quick union needs N operations
-            if (Connected(p, q))
-                return;
+            ////We don't check that because the find in the quick union needs N operations
+            //if (Connected(p, q))
+            //    return;
 
             int rootP = GetRoot(p);
             int rootQ = GetRoot(q);
-            
+
             id[rootP] = rootQ;
+        }
+
+
+        //to union two elements, we change the pointer of the root of P to point to the root of Q.
+        //We want to merge the smaller tree to the bigger tree
+        public void WeightedUnion(int p, int q)
+        {
+
+            ////We don't check that because the find in the quick union needs N operations
+            //if (Connected(p, q))
+            //    return;
+
+            int rootP = GetRoot(p);
+            int rootQ = GetRoot(q);
+
+            if (rootP == rootQ)
+                return;
+
+            //We want to merge the smaller tree to the bigger tree
+
+            //If the tree that starts on root of P is smaller than the tree that starts on root of Q
+            //we link the tree root on P to the Root of Q
+            if (sizeRoot[rootP] < sizeRoot[rootQ])
+            {
+                id[rootP] = rootQ;
+                sizeRoot[rootQ] += sizeRoot[rootP];
+            }
+            //If the tree that starts on root of Q is smaller than the tree that starts on root of P
+            //we link the tree root on Q to the Root of P
+            else
+            {
+                id[rootQ] = rootP;
+                sizeRoot[rootP] += sizeRoot[rootQ];
+            }
+        }
+
+        private int GetRootWithFlattering(int node)
+        {
+            while (node != id[node])
+            {
+                id[node] = id[id[node]];
+                node = id[node];
+            }
+
+            return node;
         }
     }
 }
